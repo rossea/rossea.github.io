d9d7897cd468bd451e7bafb186e0c7dcef75e640
rossea <lruoxi@hotmail.com>
2024-10-26 20:05:00 +0800

    Add files via upload

---
+++ psm/assets/app.12d4ded5.js
@@ -0,0 +1,1 @@
+import{G as a}from"./index.0c40de0b.js";const s=a("app",{state:()=>({printPreviewPapers:[]}),actions:{navigateToPrint(t,e,p){this.$patch(r=>{r.printPreviewPapers=p,t.push({path:"/print",query:{fileName:e}})})}}});export{s as u};
