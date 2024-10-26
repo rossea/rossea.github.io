d67032fb74b8c39781ec1433a0e6528987e3a8a4
rossea <dxln@163.com>
2024-10-26 20:02:22 +0800

    Delete psm directory

--- psm/assets/app.a6a6a79f.js
+++
@@ -1,1 +0,0 @@
-import{G as a}from"./index.4a12e8cb.js";const s=a("app",{state:()=>({printPreviewPapers:[]}),actions:{navigateToPrint(t,e,p){this.$patch(r=>{r.printPreviewPapers=p,t.push({path:"/print",query:{fileName:e}})})}}});export{s as u};
